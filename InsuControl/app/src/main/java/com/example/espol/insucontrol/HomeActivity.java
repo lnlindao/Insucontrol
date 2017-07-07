dbc762d7faa904609f1a3c0769a0e2b9e5b22a53
lnlindao <lnlindao@hotmail.com>
2017-07-06 23:06:34 -0500

    Pantallas hasta Registro completo
    Falta completar pantalla principal

--- InsuControl/app/src/main/java/com/example/espol/insucontrol/HomeActivity.java
+++ InsuControl/app/src/main/java/com/example/espol/insucontrol/HomeActivity.java
@@ -2,8 +2,11 @@ package com.example.espol.insucontrol;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 
 public class HomeActivity extends AppCompatActivity {
+
+    CardView card_gs, card_ch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
