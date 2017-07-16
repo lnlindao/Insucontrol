1506d7c6dce14a7d2ad9c94088cda3e1d89f4a88
lnlindao <lnlindao@hotmail.com>
2017-07-16 18:51:28 -0500

    Funcional hasta lista alimentos diseño

--- InsuControl/app/src/main/java/com/example/espol/insucontrol/ListAlimentos.java
+++ InsuControl/app/src/main/java/com/example/espol/insucontrol/ListAlimentos.java
@@ -14,6 +14,7 @@ import android.widget.Toast;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.example.espol.insucontrol.Conexion.Constantes;
 import com.example.espol.insucontrol.Conexion.VolleySingleton;
@@ -59,6 +60,14 @@ public class ListAlimentos extends AppCompatActivity implements AdapterView.OnIt
 
         JSONObject jobject = new JSONObject(map);
         Log.d("TAG", jobject.toString());
+
+
+
+
+
+
+
+
 
         // Actualizar datos en el servidor
         VolleySingleton.getInstance(getApplicationContext()).addToRequestQueue(
